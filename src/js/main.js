8b492b9843ebe377a8146d2d3ecdc159af2580ac
Joie <uniquek1018@naver.com>
2019-09-22 23:28:57 +0900

    dodo6

--- src/js/main.js
+++ src/js/main.js
@@ -25,6 +25,27 @@ $(document).ready(function(){
     $("#fourth").show();
   });
 
+  $(function() {
+    var boxes = $(".drag");
+
+    // Set up click handlers for each box
+    boxes.click(function() {
+      var el = $(this), // The box that was clicked
+        max = 0;
+
+      // Find the highest z-index
+      boxes.each(function() {
+        // Find the current z-index value
+        var z = parseInt( $( this ).css( "z-index" ), 10 );
+        // Keep either the current max, or the current z-index, whichever is higher
+        max = Math.max( max, z );
+      });
+
+      // Set the box that was clicked to the highest z-index plus one
+      el.css("z-index", max + 1 );
+    });
+  });
+
 
 
   $(document).mouseup(function (e)
